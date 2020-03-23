b61c2effa4614aea4c5d8a9b41d738ef3a339e1d
andrew <andrew@ska.ac.za>
2020-03-23 10:41:59 +0200

    [CBFTASKS-707] directory structure simplification
    Too many subdirectories needing to be included in Matlab path

--- startup.m
+++ startup.m
@@ -1,31 +1,46 @@
 warning off Simulink:Masking:BlockNotMasked
+warning off Simulink:SL_SaveWithParameterizedLinks_Warning
+warning off Simulink:Engine:SaveWithParameterizedLinks_Warning
+warning off Simulink:Engine:SaveWithDisabledLinks_Warning
 warning off Simulink:Commands:LoadMdlParameterizedLink
 warning off Simulink:Commands:OutputNotConnected
 warning on verbose
-addpath([getenv('XILINX_PATH'), '/ISE/sysgen/util/']);
-addpath([getenv('XILINX_PATH'), '/ISE/sysgen/bin/lin64']);
-addpath([getenv('MLIB_DEVEL_PATH'), '/casper_library']);
-addpath([getenv('MLIB_DEVEL_PATH'), '/xps_library']);
-xlAddSysgen([getenv('XILINX_PATH'), '/ISE'])
+
+jasper_backend = getenv('JASPER_BACKEND');
+
+%if vivado is to be used
+if strcmp(jasper_backend, 'vivado') || isempty(jasper_backend)
+  disp('Starting Vivado Sysgen')
+  addpath([getenv('MLIB_DEVEL_PATH'), '/casper_library']);
+  addpath([getenv('MLIB_DEVEL_PATH'), '/xps_library']);
+  addpath([getenv('MLIB_DEVEL_PATH'), '/jasper_library']);
+%if ISE is to be used  
+elseif strcmp(jasper_backend, 'ise')
+  disp('Starting ISE Sysgen')
+  addpath([getenv('XILINX_PATH'), '/ISE/sysgen/util/']);
+  addpath([getenv('XILINX_PATH'), '/ISE/sysgen/bin/lin64']);
+  addpath([getenv('MLIB_DEVEL_PATH'), '/casper_library']);
+  addpath([getenv('MLIB_DEVEL_PATH'), '/xps_library']);
+  addpath([getenv('MLIB_DEVEL_PATH'), '/jasper_library']);
+  xlAddSysgen([getenv('XILINX_PATH'), '/ISE'])
+  sysgen_startup
+else
+  fprintf('Unknown JASPER_BACKEND ''%s''\n', jasper_library);
+  % Hopefully helpful in this case
+  addpath([getenv('MLIB_DEVEL_PATH'), '/casper_library']);
+  addpath([getenv('MLIB_DEVEL_PATH'), '/xps_library']);
+  addpath([getenv('MLIB_DEVEL_PATH'), '/jasper_library']);
+end
+
 %uti stuff
-uti_root = '/home/uti/matlab';
+uti_root = getenv('UTI_PATH');
+if isempty(uti_root)
+  uti_startup_dir = './'
+end
+
 addpath([uti_root,'/tools']);
-addpath([uti_root,'/tools/misc']);
-addpath([uti_root,'/tools/par']);
-addpath([uti_root,'/tools/data']);
-addpath([uti_root,'/tools/inport']);
-addpath([uti_root,'/tools/outport']);
-addpath([uti_root,'/tools/block']);
-addpath([uti_root,'/tools/model']);
-addpath([uti_root,'/tools/bbox']);
-addpath([uti_root,'/tools/sources']);
-addpath([uti_root,'/tools/uti_library']);
-addpath([uti_root,'/libs/casper']);
-addpath([uti_root,'/libs/casper/pfb_fir']);
-addpath([uti_root,'/libs/casper/fft']);
-addpath([uti_root,'/apps/casper']);
+addpath([uti_root,'/libs']);
+addpath([uti_root,'/apps']);
 %generic start
-sysgen_startup
 load_system('casper_library');
 load_system('xps_library');
-cd([uti_root,'/apps/casper']);
